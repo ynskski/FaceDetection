c4fbfcd2ca3314ab424a494b0de258726b24bec8
Yunosuke Sakai <qmkns246@gmail.com>
2020-10-13 23:07:04 +0900

    Implement setupPreviewView method

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var previewView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private func setupPreviewView() {
+        previewView = UIView()
+        previewView?.backgroundColor = .black
+        previewView?.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(previewView!)
+        
+        previewView?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        previewView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        previewView?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        previewView?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 
